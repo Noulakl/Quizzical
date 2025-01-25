a75fc1bdddedfd8e0223e0dd0ea34c92d2ccd408
Noulakl <andriatsaranolalaina@gmail.com>
2025-01-25 15:26:39 +0300

    made the choices more funny
    changed the onClick on startPage to be onChange to see if it will respond on  mobile deivces

--- Pages/StartPage.jsx
+++ Pages/StartPage.jsx
@@ -22,54 +22,56 @@ export default function StartPage(){
             <h1 className="title">Quizzical</h1>
             <h2 className="title">Challenge yourself with random quizzes</h2>
             <form id="settings" className="settings">
-                <select name="trivia_category" id="category" className="quizz_setting" onClick={checkIfReady} ref={categoryEl}>
+                <select name="trivia_category" id="category" className="quizz_setting" onChange={checkIfReady} ref={categoryEl}>
                     <option value="choose" className="choose">--- Choose category ---</option>
 
                     <optgroup label="General knowledge 🌍">
 
-                        <option value="any"> Random </option>
-                        <option value="9"> General Knowledge </option>
-                        <option value="20"> Mythology </option>
-                        <option value="21"> Sports </option>
-                        <option value="22"> Geography </option>
-                        <option value="23"> History </option>
-                        <option value="24"> Politics </option>
-                        <option value="25"> Art </option>
-                        <option value="26"> Celebrities </option>
-                        <option value="27"> Animals </option>
-                        <option value="28"> Vehicles </option>
+                        <option value="any">🌎 Random</option>
+                        <option value="9">📚 General Knowledge</option>
+                        <option value="20">🦸‍♂️ Mythology</option>
+                        <option value="21">🏅 Sports</option>
+                        <option value="22">🌍 Geography</option>
+                        <option value="23">🏛️ History</option>
+                        <option value="24">🏛️ Politics</option>
+                        <option value="25">🎨 Art</option>
+                        <option value="26">🌟 Celebrities</option>
+                        <option value="27">🐾 Animals</option>
+                        <option value="28">🚗 Vehicles</option>
                         
                     </optgroup>
                     <optgroup label="Entertainment 🎭">
 
-                        <option value="10"> Books </option>
-                        <option value="11"> Film </option>
-                        <option value="12"> Music </option>
-                        <option value="13"> Musicals and Theatres </option>
-                        <option value="14"> Television </option>
-                        <option value="15"> Video Games </option>
-                        <option value="16"> Board Games </option>
-                        <option value="29"> Comics </option>
-                        <option value="31"> Japanese Anime and Manga </option>
-                        <option value="32"> Cartoon and Animations </option>
+                        <option value="10">📖 Books</option>
+                        <option value="11">🎬 Film</option>
+                        <option value="12">🎶 Music</option>
+                        <option value="13">🎭 Musicals and Theatres</option>
+                        <option value="14">📺 Television</option>
+                        <option value="15">🎮 Video Games</option>
+                        <option value="16">🎲 Board Games</option>
+                        <option value="29">🦸 Comics</option>
+                        <option value="31">🎥 Anime and Manga</option>
+                        <option value="32">📺 Cartoon and Animations</option>
 
                     </optgroup>
                     <optgroup label="Science 🧪">
 
-                        <option value="17"> Science and Nature </option>
-                        <option value="18"> Computers </option>
-                        <option value="19"> Mathematics </option>
-                        <option value="30"> Gadgets </option>
+                        <option value="17">🔬 Science and Nature</option>
+                        <option value="18">💻 Computers</option>
+                        <option value="19">📐 Mathematics</option>
+                        <option value="30">📱 Gadgets</option>
 
                     </optgroup>
                 </select>
 
-                <select name="trivia_difficulty" id="diffuculty" className="quizz_setting" onClick={checkIfReady} ref={difficultyEl}>
+                <select name="trivia_difficulty" id="diffuculty" className="quizz_setting" onChange={checkIfReady} ref={difficultyEl}>
                     <option value="choose" className="choose">--- Choose difficulty ---</option>
-                    <option value="any"> Random 😎</option>
-                    <option value="easy"> Noobs 😗</option>
-                    <option value="medium"> Pro 🤓</option>
-                    <option value="hard"> God 🧐</option>
+
+                    <option value="any">🎲 Random 😎</option>
+                    <option value="easy">👶 Noobs 😗</option>
+                    <option value="medium">💪 Pro 🤓</option>
+                    <option value="hard">🧠 God 🧐</option>
+                    
                 </select>
             </form>
 
